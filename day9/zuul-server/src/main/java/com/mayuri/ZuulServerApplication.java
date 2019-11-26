0e368de9ddda71c40ba81bdd76866d64e638898a
mayur <mayur@WINDOWS-C54NIUR>
2019-11-26 20:23:17 +0530

    day9 assignment

--- day9/zuul-server/src/main/java/com/mayuri/ZuulServerApplication.java
+++ day9/zuul-server/src/main/java/com/mayuri/ZuulServerApplication.java
@@ -2,13 +2,14 @@ package com.mayuri;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @RefreshScope
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ZuulServerApplication {
 
 	public static void main(String[] args) {
